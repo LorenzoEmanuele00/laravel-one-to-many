17d3972ea64963b158b5faab425835bc4af39375
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2024-01-26 22:31:10 +0100

    created seeder for types table and fix projects migration for type_id

--- database/migrations/2024_01_26_210314_create_types_table.php
+++ database/migrations/2024_01_26_210314_create_types_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->name();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
